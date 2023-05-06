6063734dc9dcc3e8d3f4c3115d165189553f2770
okimoki-soomni <okimoki-soomni@gmail.com>
2023-05-06 16:57:02 +0900

    initial commit

---
+++ src/main/java/com/kabang/KabangApplication.java
@@ -0,0 +1,13 @@
+package com.kabang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KabangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KabangApplication.class, args);
+	}
+
+}
